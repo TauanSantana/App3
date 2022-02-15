50d9332559bf308b458635f8ffda3ba991c8d3b2
CETI - Tauan Santana da Silva <2838@appai.org.br>
2022-02-15 11:51:39 -0300

    Projeto inicial

---
+++ App3/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<string> Sucesso()
+        {
+            Thread.Sleep(new Random().Next(600, 2000));
+            return "";
+        }
+
+        [HttpPost]
+        public async Task Problema()
+        {
+            int rnd = new Random().Next(1, 20);
+#if !DEBUG
+            if (rnd % 3 == 0)
+                throw new Exception("Erro fake - Falha ao conectar ao banco de dados");
+#endif
+            Thread.Sleep(new Random().Next(600, 2000));
+        }
+    }
+}
---
+++ App3/Scripts/App.js
@@ -0,0 +1,32 @@
+﻿var app = {}
+
+app.totalFalhas = 0;
+app.totalSucesso = 0;
+app.chamadaSucesso = function () {
+    let response = fetch('/home/sucesso').then(function (response) {
+        app.tratarRetorno(response);
+    });
+}
+
+app.chamadaProblema = function () {
+    let response = fetch('/home/problema', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: null,
+    }).then(function (response) {
+        app.tratarRetorno(response);
+    });
+}
+
+app.tratarRetorno = function (response) {
+    if (response.status != 200) {
+        app.totalFalhas = ++app.totalFalhas;
+    } else {
+        app.totalSucesso = ++app.totalSucesso;
+    }
+
+    $("#div-totalsucesso").text(app.totalSucesso);
+    $("#div-totalfalhas").text(app.totalFalhas)
+}
